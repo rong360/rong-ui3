3d09441338129c6e03d94d9edc2a200c1ecf5c40
liwenjing <liwenjng@rong360.com>
2023-08-02 17:18:25 +0800

    del unused lib

--- src/sites/doc/views/home/index.vue
+++ src/sites/doc/views/home/index.vue
@@ -1,15 +1,5 @@
 <template>
   <div> this is home </div>
-  <!-- <Dialog v-model:show="showDialog">sdfsf</Dialog> -->
-  <!-- <van-button>sfsdfs</van-button>
-  <nut-button>nut-button</nut-button> -->
 </template>
 
-<script setup>
-// import { ref } from 'vue';
-// import Dialog from '../../../../dist/lib/dialog';
-// import FlexFixed from '../../../../dist/flex-fixed';
-
-// const showDialog = ref(true);
-// import VanButton from 'vant/lib/button';
-</script>
+<script setup></script>
