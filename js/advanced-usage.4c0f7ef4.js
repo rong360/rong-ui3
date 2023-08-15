f1afd732804f5ca683928e22e9d29308219723bf
liwenjing <liwenjng@rong360.com>
2023-08-15 16:16:48 +0800

    doc update

--- js/advanced-usage.4c0f7ef4.js
+++ js/advanced-usage.4c0f7ef4.js
@@ -10,7 +10,7 @@ app.`),u("span",{class:"hljs-title function_"},"use"),s("("),u("span",{class:"hl
 `),u("span",{class:"hljs-comment"},"// \u65B9\u5F0F\u4E8C. \u901A\u8FC7 app.component \u6CE8\u518C"),s(`
 `),u("span",{class:"hljs-comment"},"// \u6CE8\u518C\u5B8C\u6210\u540E\uFF0C\u5728\u6A21\u677F\u4E2D\u901A\u8FC7 <r-button> \u6807\u7B7E\u6765\u4F7F\u7528\u6309\u94AE\u7EC4\u4EF6"),s(`
 app.`),u("span",{class:"hljs-title function_"},"component"),s("("),u("span",{class:"hljs-title class_"},"Button"),s("."),u("span",{class:"hljs-property"},"name"),s(", "),u("span",{class:"hljs-title class_"},"Button"),s(`);
-`)])],-1),D=u("h5",null,"\u5C40\u90E8\u6CE8\u518C",-1),g=u("p",null,"\u5C40\u90E8\u6CE8\u518C\u540E\uFF0C\u4F60\u53EF\u4EE5\u5728\u5F53\u524D\u7EC4\u4EF6\u4E2D\u4F7F\u7528\u6CE8\u518C\u7684 Vant \u7EC4\u4EF6\u3002",-1),A=u("pre",null,[u("code",{class:"language-js"},[s(`
+`)])],-1),D=u("h5",null,"\u5C40\u90E8\u6CE8\u518C",-1),g=u("p",null,"\u5C40\u90E8\u6CE8\u518C\u540E\uFF0C\u4F60\u53EF\u4EE5\u5728\u5F53\u524D\u7EC4\u4EF6\u4E2D\u4F7F\u7528\u6CE8\u518C\u7684 RongUi3 \u7EC4\u4EF6\u3002",-1),A=u("pre",null,[u("code",{class:"language-js"},[s(`
 `),u("span",{class:"hljs-keyword"},"import"),s(" { "),u("span",{class:"hljs-title class_"},"Button"),s(" } "),u("span",{class:"hljs-keyword"},"from"),s(),u("span",{class:"hljs-string"},"'rong-ui3'"),s(`;
 
 `),u("span",{class:"hljs-keyword"},"export"),s(),u("span",{class:"hljs-keyword"},"default"),s(` {
@@ -19,21 +19,21 @@ app.`),u("span",{class:"hljs-title function_"},"component"),s("("),u("span",{cla
   },
 };
 
-`)])],-1),f=u("h5",null,"script setup",-1),v=u("p",null,[s("\u5728 "),u("code",{class:""},"<script setup>"),s(" \u4E2D\u53EF\u4EE5\u76F4\u63A5\u4F7F\u7528 Vant \u7EC4\u4EF6\uFF0C\u4E0D\u9700\u8981\u8FDB\u884C\u7EC4\u4EF6\u6CE8\u518C\u3002")],-1),x=u("pre",null,[u("code",{class:"language-xml"},[u("span",{class:"hljs-tag"},[s("<"),u("span",{class:"hljs-name"},"script"),s(),u("span",{class:"hljs-attr"},"setup"),s(">")]),u("span",{class:"language-javascript"},[s(`
+`)])],-1),f=u("h5",null,"script setup",-1),x=u("p",null,[s("\u5728 "),u("code",{class:""},"<script setup>"),s(" \u4E2D\u53EF\u4EE5\u76F4\u63A5\u4F7F\u7528 RongUi3 \u7EC4\u4EF6\uFF0C\u4E0D\u9700\u8981\u8FDB\u884C\u7EC4\u4EF6\u6CE8\u518C\u3002")],-1),v=u("pre",null,[u("code",{class:"language-xml"},[u("span",{class:"hljs-tag"},[s("<"),u("span",{class:"hljs-name"},"script"),s(),u("span",{class:"hljs-attr"},"setup"),s(">")]),u("span",{class:"language-javascript"},[s(`
   `),u("span",{class:"hljs-keyword"},"import"),s(" { "),u("span",{class:"hljs-title class_"},"Button"),s(" } "),u("span",{class:"hljs-keyword"},"from"),s(),u("span",{class:"hljs-string"},"'rong-ui3'"),s(`;
 `)]),u("span",{class:"hljs-tag"},[s("</"),u("span",{class:"hljs-name"},"script"),s(">")]),s(`
 
 `),u("span",{class:"hljs-tag"},[s("<"),u("span",{class:"hljs-name"},"template"),s(">")]),s(`
   `),u("span",{class:"hljs-tag"},[s("<"),u("span",{class:"hljs-name"},"Button"),s(" />")]),s(`
 `),u("span",{class:"hljs-tag"},[s("</"),u("span",{class:"hljs-name"},"template"),s(">")]),s(`
-`)])],-1),w=u("h5",null,"JSX/TSX",-1),b=u("p",null,"\u5728 JSX \u548C TSX \u4E2D\u53EF\u4EE5\u76F4\u63A5\u4F7F\u7528 Vant \u7EC4\u4EF6\uFF0C\u4E0D\u9700\u8981\u8FDB\u884C\u7EC4\u4EF6\u6CE8\u518C\u3002",-1),y=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-keyword"},"import"),s(" { "),u("span",{class:"hljs-title class_"},"Button"),s(" } "),u("span",{class:"hljs-keyword"},"from"),s(),u("span",{class:"hljs-string"},"'rong-ui3'"),s(`;
+`)])],-1),w=u("h5",null,"JSX/TSX",-1),b=u("p",null,"\u5728 JSX \u548C TSX \u4E2D\u53EF\u4EE5\u76F4\u63A5\u4F7F\u7528 RongUi3 \u7EC4\u4EF6\uFF0C\u4E0D\u9700\u8981\u8FDB\u884C\u7EC4\u4EF6\u6CE8\u518C\u3002",-1),y=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-keyword"},"import"),s(" { "),u("span",{class:"hljs-title class_"},"Button"),s(" } "),u("span",{class:"hljs-keyword"},"from"),s(),u("span",{class:"hljs-string"},"'rong-ui3'"),s(`;
 
 `),u("span",{class:"hljs-keyword"},"export"),s(),u("span",{class:"hljs-keyword"},"default"),s(` {
   `),u("span",{class:"hljs-title function_"},"render"),s("("),u("span",{class:"hljs-params"}),s(`) {
     `),u("span",{class:"hljs-keyword"},"return"),s(),u("span",{class:"language-xml"},[u("span",{class:"hljs-tag"},[s("<"),u("span",{class:"hljs-name"},"Button"),s(" />")])]),s(`;
   },
 };
-`)])],-1),k=u("h3",null,"\u6D4F\u89C8\u5668\u9002\u914D",-1),S={class:"card"},V=u("h4",null,"Rem \u5E03\u5C40\u9002\u914D",-1),X=u("p",null,"\u5982\u679C\u9700\u8981\u4F7F\u7528 rem \u5355\u4F4D\u8FDB\u884C\u9002\u914D\uFF0C\u63A8\u8350\u4F7F\u7528\u4EE5\u4E0B\u4E24\u4E2A\u5DE5\u5177\uFF1A",-1),R=u("ol",null,[u("li",null,[u("code",{class:""},"postcss-pxtorem"),s(" \u662F\u4E00\u6B3E PostCSS \u63D2\u4EF6\uFF0C\u7528\u4E8E\u5C06 px \u5355\u4F4D\u8F6C\u5316\u4E3A rem \u5355\u4F4D")]),u("li",null,[u("code",{class:""},"lib-flexible"),s(" \u7528\u4E8E\u8BBE\u7F6E rem \u57FA\u51C6\u503C")])],-1),P=u("h4",null,"PostCSS \u793A\u4F8B\u914D\u7F6E",-1),T=u("p",null,"\u4E0B\u9762\u63D0\u4F9B\u4E86\u4E00\u4EFD\u57FA\u672C\u7684 PostCSS \u793A\u4F8B\u914D\u7F6E\uFF0C\u53EF\u4EE5\u5728\u6B64\u914D\u7F6E\u7684\u57FA\u7840\u4E0A\u6839\u636E\u9879\u76EE\u9700\u6C42\u8FDB\u884C\u4FEE\u6539\u3002",-1),$=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
+`)])],-1),k=u("h3",null,"\u6D4F\u89C8\u5668\u9002\u914D",-1),S={class:"card"},R=u("h4",null,"Rem \u5E03\u5C40\u9002\u914D",-1),V=u("p",null,"\u5982\u679C\u9700\u8981\u4F7F\u7528 rem \u5355\u4F4D\u8FDB\u884C\u9002\u914D\uFF0C\u63A8\u8350\u4F7F\u7528\u4EE5\u4E0B\u4E24\u4E2A\u5DE5\u5177\uFF1A",-1),X=u("ol",null,[u("li",null,[u("code",{class:""},"postcss-pxtorem"),s(" \u662F\u4E00\u6B3E PostCSS \u63D2\u4EF6\uFF0C\u7528\u4E8E\u5C06 px \u5355\u4F4D\u8F6C\u5316\u4E3A rem \u5355\u4F4D")]),u("li",null,[u("code",{class:""},"lib-flexible"),s(" \u7528\u4E8E\u8BBE\u7F6E rem \u57FA\u51C6\u503C")])],-1),P=u("h4",null,"PostCSS \u793A\u4F8B\u914D\u7F6E",-1),T=u("p",null,"\u4E0B\u9762\u63D0\u4F9B\u4E86\u4E00\u4EFD\u57FA\u672C\u7684 PostCSS \u793A\u4F8B\u914D\u7F6E\uFF0C\u53EF\u4EE5\u5728\u6B64\u914D\u7F6E\u7684\u57FA\u7840\u4E0A\u6839\u636E\u9879\u76EE\u9700\u6C42\u8FDB\u884C\u4FEE\u6539\u3002",-1),U=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
 `),u("span",{class:"hljs-variable language_"},"module"),s("."),u("span",{class:"hljs-property"},"exports"),s(` = {
   `),u("span",{class:"hljs-attr"},"plugins"),s(`: {
     `),u("span",{class:"hljs-string"},"'postcss-pxtorem'"),s(`: {
@@ -42,20 +42,20 @@ app.`),u("span",{class:"hljs-title function_"},"component"),s("("),u("span",{cla
     },
   },
 };
-`)])],-1),J=u("blockquote",null,[u("p",null,"Tips: \u5728\u914D\u7F6E postcss-pxtorem \u65F6\uFF0C\u540C\u6837\u5E94\u907F\u514D ignore node_modules \u76EE\u5F55\uFF0C\u5426\u5219\u4F1A\u5BFC\u81F4 Vant \u6837\u5F0F\u65E0\u6CD5\u88AB\u7F16\u8BD1\u3002")],-1),L=u("h4",null,"\u5176\u4ED6\u8BBE\u8BA1\u7A3F\u5C3A\u5BF8",-1),N=u("p",null,[s("\u5982\u679C\u8BBE\u8BA1\u7A3F\u7684\u5C3A\u5BF8\u4E0D\u662F 375\uFF0C\u800C\u662F 750 \u6216\u5176\u4ED6\u5927\u5C0F\uFF0C\u53EF\u4EE5\u5C06 "),u("code",{class:""},"rootValue"),s(" \u914D\u7F6E\u8C03\u6574\u4E3A")],-1),I=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
+`)])],-1),$=u("blockquote",null,[u("p",null,"Tips: \u5728\u914D\u7F6E postcss-pxtorem \u65F6\uFF0C\u540C\u6837\u5E94\u907F\u514D ignore node_modules \u76EE\u5F55\uFF0C\u5426\u5219\u4F1A\u5BFC\u81F4 RongUi3 \u6837\u5F0F\u65E0\u6CD5\u88AB\u7F16\u8BD1\u3002")],-1),J=u("h4",null,"\u5176\u4ED6\u8BBE\u8BA1\u7A3F\u5C3A\u5BF8",-1),L=u("p",null,[s("\u5982\u679C\u8BBE\u8BA1\u7A3F\u7684\u5C3A\u5BF8\u4E0D\u662F 375\uFF0C\u800C\u662F 750 \u6216\u5176\u4ED6\u5927\u5C0F\uFF0C\u53EF\u4EE5\u5C06 "),u("code",{class:""},"rootValue"),s(" \u914D\u7F6E\u8C03\u6574\u4E3A")],-1),N=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
 `),u("span",{class:"hljs-variable language_"},"module"),s("."),u("span",{class:"hljs-property"},"exports"),s(` = {
   `),u("span",{class:"hljs-attr"},"plugins"),s(`: {
     `),u("span",{class:"hljs-comment"},"// postcss-pxtorem \u63D2\u4EF6\u7684\u7248\u672C\u9700\u8981 >= 5.0.0"),s(`
     `),u("span",{class:"hljs-string"},"'postcss-pxtorem'"),s(`: {
       `),u("span",{class:"hljs-title function_"},"rootValue"),s("("),u("span",{class:"hljs-params"},"{ file }"),s(`) {
-        `),u("span",{class:"hljs-keyword"},"return"),s(" file."),u("span",{class:"hljs-title function_"},"indexOf"),s("("),u("span",{class:"hljs-string"},"'vant'"),s(") !== -"),u("span",{class:"hljs-number"},"1"),s(" ? "),u("span",{class:"hljs-number"},"37.5"),s(" : "),u("span",{class:"hljs-number"},"75"),s(`;
+        `),u("span",{class:"hljs-keyword"},"return"),s(" file."),u("span",{class:"hljs-title function_"},"indexOf"),s("("),u("span",{class:"hljs-string"},"'rong-ui3'"),s(") !== -"),u("span",{class:"hljs-number"},"1"),s(" ? "),u("span",{class:"hljs-number"},"37.5"),s(" : "),u("span",{class:"hljs-number"},"75"),s(`;
       },
       `),u("span",{class:"hljs-attr"},"propList"),s(": ["),u("span",{class:"hljs-string"},"'*'"),s(`],
     },
   },
 };
 
-`)])],-1),O={class:"card"},U=a('<h4>Viewport \u5E03\u5C40</h4><p>rong-ui3 \u9ED8\u8BA4\u4F7F\u7528 <code class="">px</code> \u4F5C\u4E3A\u6837\u5F0F\u5355\u4F4D\uFF0C\u5982\u679C\u9700\u8981\u4F7F\u7528 <code class="">viewport</code> \u5355\u4F4D (vw, vh, vmin, vmax)\uFF0C\u63A8\u8350\u4F7F\u7528 <code class="">postcss-px-to-viewport</code> \u8FDB\u884C\u8F6C\u6362\u3002<br><br> postcss-px-to-viewport \u662F\u4E00\u6B3E PostCSS \u63D2\u4EF6\uFF0C\u7528\u4E8E\u5C06 px \u5355\u4F4D\u8F6C\u5316\u4E3A vw/vh \u5355\u4F4D\u3002</p><br><h4>PostCSS \u793A\u4F8B\u914D\u7F6E</h4><p>\u4E0B\u9762\u63D0\u4F9B\u4E86\u4E00\u4EFD\u57FA\u672C\u7684 PostCSS \u793A\u4F8B\u914D\u7F6E\uFF0C\u53EF\u4EE5\u5728\u6B64\u914D\u7F6E\u7684\u57FA\u7840\u4E0A\u6839\u636E\u9879\u76EE\u9700\u6C42\u8FDB\u884C\u4FEE\u6539\u3002</p>',5),W=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
+`)])],-1),I={class:"card"},O=a('<h4>Viewport \u5E03\u5C40</h4><p>rong-ui3 \u9ED8\u8BA4\u4F7F\u7528 <code class="">px</code> \u4F5C\u4E3A\u6837\u5F0F\u5355\u4F4D\uFF0C\u5982\u679C\u9700\u8981\u4F7F\u7528 <code class="">viewport</code> \u5355\u4F4D (vw, vh, vmin, vmax)\uFF0C\u63A8\u8350\u4F7F\u7528 <code class="">postcss-px-to-viewport</code> \u8FDB\u884C\u8F6C\u6362\u3002<br><br> postcss-px-to-viewport \u662F\u4E00\u6B3E PostCSS \u63D2\u4EF6\uFF0C\u7528\u4E8E\u5C06 px \u5355\u4F4D\u8F6C\u5316\u4E3A vw/vh \u5355\u4F4D\u3002</p><br><h4>PostCSS \u793A\u4F8B\u914D\u7F6E</h4><p>\u4E0B\u9762\u63D0\u4F9B\u4E86\u4E00\u4EFD\u57FA\u672C\u7684 PostCSS \u793A\u4F8B\u914D\u7F6E\uFF0C\u53EF\u4EE5\u5728\u6B64\u914D\u7F6E\u7684\u57FA\u7840\u4E0A\u6839\u636E\u9879\u76EE\u9700\u6C42\u8FDB\u884C\u4FEE\u6539\u3002</p>',5),W=u("pre",null,[u("code",{class:"language-js"},[u("span",{class:"hljs-comment"},"// postcss.config.js"),s(`
 `),u("span",{class:"hljs-variable language_"},"module"),s("."),u("span",{class:"hljs-property"},"exports"),s(` = {
   `),u("span",{class:"hljs-attr"},"plugins"),s(`: {
     `),u("span",{class:"hljs-string"},"'postcss-px-to-viewport'"),s(`: {
@@ -89,21 +89,21 @@ export default {
   },
 };
 
-`},{default:t(()=>[A]),_:1}),f,v,l(n,{code:`<script setup>
+`},{default:t(()=>[A]),_:1}),f,x,l(n,{code:`<script setup>
   import { Button } from 'rong-ui3';
 <\/script>
 
 <template>
   <Button />
 </template>
-`},{default:t(()=>[x]),_:1}),w,b,l(n,{code:`import { Button } from 'rong-ui3';
+`},{default:t(()=>[v]),_:1}),w,b,l(n,{code:`import { Button } from 'rong-ui3';
 
 export default {
   render() {
     return <Button />;
   },
 };
-`},{default:t(()=>[y]),_:1})]),k,u("div",S,[V,X,R,P,T,l(n,{code:`// postcss.config.js
+`},{default:t(()=>[y]),_:1})]),k,u("div",S,[R,V,X,P,T,l(n,{code:`// postcss.config.js
 module.exports = {
   plugins: {
     'postcss-pxtorem': {
@@ -112,20 +112,20 @@ module.exports = {
     },
   },
 };
-`},{default:t(()=>[$]),_:1}),J,L,N,l(n,{code:`// postcss.config.js
+`},{default:t(()=>[U]),_:1}),$,J,L,l(n,{code:`// postcss.config.js
 module.exports = {
   plugins: {
     // postcss-pxtorem \u63D2\u4EF6\u7684\u7248\u672C\u9700\u8981 >= 5.0.0
     'postcss-pxtorem': {
       rootValue({ file }) {
-        return file.indexOf('vant') !== -1 ? 37.5 : 75;
+        return file.indexOf('rong-ui3') !== -1 ? 37.5 : 75;
       },
       propList: ['*'],
     },
   },
 };
 
-`},{default:t(()=>[I]),_:1})]),u("div",O,[U,l(n,{code:`// postcss.config.js
+`},{default:t(()=>[N]),_:1})]),u("div",I,[O,l(n,{code:`// postcss.config.js
 module.exports = {
   plugins: {
     'postcss-px-to-viewport': {
